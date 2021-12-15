1f873ec26f3edbfd5e875910dc3df4c269d3a344
Marcus Ambichl <marcus.ambichl@sanid.com>
2021-12-15 11:16:06 +0100

    Rewired Setup

--- Assets/Scripts/Spy.cs
+++ Assets/Scripts/Spy.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Rewired;
+using UnityEngine;
 
 public class Spy: MonoBehaviour {
     public int playerIndex;
@@ -8,10 +9,24 @@ public class Spy: MonoBehaviour {
     private MeshFilter _inventoryMesh;
     private MeshRenderer _inventoryRenderer;
     private MeshRegistry _meshRegistry;
+    
+    //Movement Stuff
+    public float moveSpeed = 3.0f;
+    private Player player; // The Rewired Player
+    private CharacterController cc;
+    private Vector3 _moveVector;
+    private bool _interact;
+    private bool _set;
+    private bool _attack;
+    private bool _map;
+    
     void Awake() {
         _inventoryMesh = transform.Find("Inventory").GetComponent<MeshFilter>();
         _inventoryRenderer = transform.Find("Inventory").GetComponent<MeshRenderer>();
         _meshRegistry = FindObjectOfType<MeshRegistry>();
+        
+        player = ReInput.players.GetPlayer(playerIndex);
+        cc = GetComponent<CharacterController>();
     }
     
     void Start() {
@@ -26,5 +41,31 @@ public class Spy: MonoBehaviour {
     public void ChangeRoom(Room targetRoom) {
         _currentRoom = targetRoom;
         transform.position = _currentRoom.GetWaypointPosition(playerIndex);
+    }
+    
+    void Update () {
+        GetInput();
+        ProcessInput();
+    }
+
+    private void GetInput() {
+        _moveVector.x = player.GetAxis("MoveHorizontal");
+        _moveVector.y = player.GetAxis("MoveVertical");
+        _interact = player.GetButtonDown("Interact");
+        _set = player.GetButtonDown("Set");
+        _attack = player.GetButtonDown("Attack");
+        _map = player.GetButtonDown("Map");
+    }
+
+    private void ProcessInput() {
+        if(_moveVector.x != 0.0f || _moveVector.y != 0.0f) {
+            cc.Move(_moveVector * moveSpeed * Time.deltaTime);
+        }
+        // Debug.Log("-------------");
+        if (_interact) Debug.Log("Interact");
+        if (_set) Debug.Log("Set");
+        if (_attack) Debug.Log("Attack");
+        if (_map) Debug.Log("Map");
+        // Debug.Log("-------------");
     }
 }
